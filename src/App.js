7bd964beea268f854561553dfd7ac8aa20f766c5
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-30 14:04:23 +0430

    feat: Header Component & Styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,31 @@ function App() {
 	return (
 		<div className="app">
 			<Header />
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
+			<h1>TEST</h1>
 		</div>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,31 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
+
 function Header() {
 	return (
 		<div>
 			<div className="header">
-				<MenuIcon />
-				<img
-					className="header__logo"
-					src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
-					alt=""
-				/>
-				<input type="text" />
-				<SearchIcon />
-				<VideoCallIcon />
-				<AppsIcon />
-				<NotificationsIcon />
-				<Avatar alt="Amir-Mohammad" src="" />
+				<div className="header__left">
+					<MenuIcon />
+					<img
+						className="header__logo"
+						src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+						alt=""
+					/>
+				</div>
+
+				<div className="header__input">
+					<input placeholder="Search" type="text" />
+					<SearchIcon className="header__inputButton" />
+				</div>
+
+				<div className="header__icon">
+					<VideoCallIcon className="header__icon" />
+					<AppsIcon className="header__icon" />
+					<NotificationsIcon className="header__icon" />
+					<Avatar alt="Amir-Mohammad" src="" />
+				</div>
 			</div>
 		</div>
 	);
